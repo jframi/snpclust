f9ad374930d3dbeb0d9aadb3baa47cdc650c46c6
jframi <jframi@gmail.com>
2018-03-21 20:58:44 +0100

    Changed message

--- R/runapps.R
+++ R/runapps.R
@@ -2,7 +2,7 @@
 runmanclust <- function() {
   appDir <- system.file("shiny-apps", "manclust", package = "snpclust")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `snpclust`.", call. = FALSE)
+    stop("Could not find app directory. Try re-installing `snpclust`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal",launch.browser = T)
